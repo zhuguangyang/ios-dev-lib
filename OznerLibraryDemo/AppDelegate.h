a08fe547568c56c87e2ae904672d6f2eea8302ef
zhiyongxu <xzyxdtxxx@gmail.com>
2015-12-09 21:42:10 +0800

    Initial Commit

---
+++ OznerLibraryDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  OznerLibraryDemo
+//
+//  Created by Zhiyongxu on 15/12/9.
+//  Copyright © 2015年 Ozner. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ OznerLibraryDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  OznerLibraryDemo
+//
+//  Created by Zhiyongxu on 15/12/9.
+//  Copyright © 2015年 Ozner. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
